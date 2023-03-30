e38831593a0685bc04dd0220fc5f59148c608a47
Colepng <gamingwithcole7@gmail.com>
2023-03-30 14:15:46 -0400

    Basic website

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::fs::{relative, FileServer};
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
+
+#[launch]
+fn rocket() -> _ {
+    // rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", FileServer::from(relative!("static")))
+}
+
